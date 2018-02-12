57fcfb9a87882028a0a9b08e1bb39b8435bf8eef
Angel <angel@192.168.211.129>
2018-02-12 15:40:53 -0800

    Splash y pantallas de login y registro hechas

---
+++ ProyectoSwift/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  ProyectoSwift
+//
+//  Created by Angel on 12/02/2018.
+//  Copyright © 2018 Javier Lopez Girela. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    //ESTA CLASE SERA PARA INTRODUCIR EL LOGIN Y EL PASSWORD ¿????
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
