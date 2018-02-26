c3ae7e0f9e3e943a09f6e4064bead5e8f3b0685f
javierlopezgirela <javi_lg2014@hotmail.com>
2018-02-26 20:36:52 +0100

    Retocado clases

---
+++ ProyectoSwift/Presentador/ViewControllerPrincipal.swift
@@ -0,0 +1,37 @@
+, UICollectionViewDelegate//
+//  ViewControllerPrincipal.swift
+//  ProyectoSwift
+//
+//  Created by Javier Lopez Girela on 26/2/18.
+//  Copyright © 2018 Javier Lopez Girela. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerPrincipal: UIViewController, UICollectionViewDelegate {
+    
+    let categories = ["Pan", "Bollería", "Croasant", "Navidad", "Otros"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
