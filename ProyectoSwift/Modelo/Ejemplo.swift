1e97205e262633f08a59ae12734fd3de9002491e
javierlopezgirela <javi_lg2014@hotmail.com>
2018-02-27 12:28:55 +0100

    modelo

---
+++ ProyectoSwift/Modelo/Ejemplo.swift
@@ -0,0 +1,11 @@
+//
+//  Ejemplo.swift
+//  ProyectoSwift
+//
+//  Created by Javier Lopez Girela on 27/2/18.
+//  Copyright © 2018 Javier Lopez Girela. All rights reserved.
+//
+
+import Foundation
+
+let hola = "Yeeh"
