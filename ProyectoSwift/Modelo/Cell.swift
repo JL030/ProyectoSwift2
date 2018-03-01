06e23f8e09ed7d767c7797f618eec5aebf5fc48e
javierlopezgirela <javi_lg2014@hotmail.com>
2018-03-01 09:48:41 +0100

    V4

---
+++ ProyectoSwift/Modelo/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Cell.swift
+//  ProyectoSwift
+//
+//  Created by Javier Lopez Girela on 1/3/18.
+//  Copyright © 2018 Javier Lopez Girela. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+}
