293fb7366263b05ba5bcb209340bc30d94304280
javierlopezgirela <javi_lg2014@hotmail.com>
2018-03-02 18:02:22 +0100

    Cache Subido

---
+++ ProyectoSwift/Modelo/Cache.swift
@@ -0,0 +1,17 @@
+//
+//  Cache.swift
+//  ProyectoSwift
+//
+//  Created by Javier Lopez Girela on 2/3/18.
+//  Copyright © 2018 Javier Lopez Girela. All rights reserved.
+//
+
+import Foundation
+
+class Cache{
+    private let url : String = "https://bbdd-javi030.c9users.io/IosPanaderia/"
+    
+    func getCategories(){
+        
+    }
+}
