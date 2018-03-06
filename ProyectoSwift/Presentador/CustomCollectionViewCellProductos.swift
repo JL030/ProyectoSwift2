409b829fea5da3c5e467b5121948e96d09e461b3
javierlopezgirela <javi_lg2014@hotmail.com>
2018-03-06 18:37:35 +0100

    Collection View Productos V1

---
+++ ProyectoSwift/Presentador/CustomCollectionViewCellProductos.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionViewCellProductos.swift
+//  ProyectoSwift
+//
+//  Created by Javier Lopez Girela on 6/3/18.
+//  Copyright © 2018 Javier Lopez Girela. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCellProductos: UICollectionViewCell {
+    @IBOutlet weak var imagenProducto: UIImageView!
+    
+    @IBOutlet weak var labelProducto: UILabel!
+}
