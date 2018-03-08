97be26d2eaa9535fcf982d93958d1add1d12111d
Angel <angel@Angels-iMac.local>
2018-03-08 01:15:46 -0800

    splash

--- ProyectoSwift/Presentador/ViewControllerMain.swift
+++ ProyectoSwift/Presentador/ViewControllerMain.swift
@@ -22,8 +22,8 @@ class ViewControllerMain: UIViewController, OnHttpResponse {
         super.viewDidLoad()
         print("TOKEN 1")
         print(tokenMain)
-        //labelprueba.text = usuario
-        
+        //labelprueba.text = usua
+        print("ID del usuario ->\(idMain)")
         // Categorias
         descargarCategorias()
         download()
