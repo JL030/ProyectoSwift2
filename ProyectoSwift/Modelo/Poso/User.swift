4848f7806abe048878dfdab5cc12bd7627507556
Angel <angel@192.168.211.129>
2018-03-07 18:47:33 -0800

    user

---
+++ ProyectoSwift/Modelo/Poso/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ProyectoSwift
+//
+//  Created by Angel on 07/03/2018.
+//  Copyright © 2018 Javier Lopez Girela. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    var id: String
+    var login: String
+    var password: String
+    
+}
